5b174354f4ebbff7abe54a48c34e2508d9761241
k.pelikhovich <kirillqa199226@gmail.com>
2025-02-19 22:06:50 +0300

    lesson_3

---
+++ lesson_3/python_basics.py
@@ -0,0 +1,17 @@
+b = None
+
+print(2 == 2)   # Вернет True или False в зависимости от данных
+
+if 1 == 1 & 1 > 0:   #   Это & and
+    print('text')
+
+if 1 == 1 | 1 < 2:   #   Это & or
+    print('yes')
+
+print('E' in 'hello')
+
+
+c = 'text'
+d = c
+
+print(c is d)
